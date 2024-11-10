91d54c297da3c603020328962c6d5f42c1798e22
Dimitar Janevski <dimitar.j27@gmail.com>
2024-11-10 03:38:24 -0700

    sms

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -4,12 +4,14 @@ import { userRouter } from "./user";
 import { recordingRouter } from "./recording";
 import { liveStreamRouter } from "./liveStream";
 import { inviteRouter } from "./invite";
+import { smsRouter } from "./sms";
 
 export const appRouter = router({
   auth: authRouter,
   invite: inviteRouter,
   liveStream: liveStreamRouter,
   recording: recordingRouter,
+  sms: smsRouter,
   user: userRouter,
 });
 
