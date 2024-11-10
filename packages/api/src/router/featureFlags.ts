c71ba4613049a1629129e586a95a42c859f691de
abodthedude25 <abdelrahman.abbas@ucalgary.ca>
2024-11-09 21:10:50 -0700

    remove post and feature flags

--- packages/api/src/router/featureFlags.ts
+++
@@ -1,7 +0,0 @@
-import { protectedProcedure, router } from "../trpc";
-
-export const featureFlagsRouter = router({
-  getFlags: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.featureFlags.findMany();
-  }),
-});
--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,17 +1,13 @@
 import { router } from "../trpc";
-import { postRouter } from "./post";
 import { authRouter } from "./auth";
-import { featureFlagsRouter } from "./featureFlags";
 import { emergencyContactRouter } from "./emergencyContacts";
 import { userRouter } from "./user";
 import { recordingRouter } from "./recording";
 import { liveStreamRouter } from "./liveStream";
-import { inviteRouter } from './invite';
+import { inviteRouter } from "./invite";
 
 export const appRouter = router({
   auth: authRouter,
-  featureFlags: featureFlagsRouter,
-  post: postRouter,
   invite: inviteRouter,
   emergencyContacts: emergencyContactRouter,
   user: userRouter,
